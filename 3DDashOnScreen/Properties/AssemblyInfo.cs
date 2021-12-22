235b54c2fc138424c2ba029147d85e9608aa92a3
rampa3 <68955305+rampa3@users.noreply.github.com>
2021-12-22 18:53:27 +0100

    Initial commit
    Initial commit, that contains the first version of the mod.

---
+++ 3DDashOnScreen/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("3D Dash On Screen")]
+[assembly: AssemblyDescription("Neos Mod Loader mod, that replaces 2D overlay dash in Screen mode with the regular one")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("3D Dash On Screen")]
+[assembly: AssemblyCopyright("Copyright ©  2021 rampa_3")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0322B2EF-7452-479D-BAE2-FCAB75033337")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,14 @@
+# 3D Dash On Screen
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that replaces 2D overlay dash in Screen mode with the regular 3D one.<br>
+Also F4 is now the edit mode toggle button since it was removed when the freeform camera was added.
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+1. Place [3DDashOnScreen.dll](https://github.com/rampa3/3DDashOnScreen/releases/latest/download/3DDashOnScreen.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+1. Start the game. If everything is working, your 2D dash will be replaced with the regular one, when using Screen mode.
+
+## Credits
+- [rampa3](https://github.com/rampa3) - original idea and research
+- [eia485](https://github.com/eia485) - added cursor parking lot and helped fix errors
+- [art0007i](https://github.com/art0007i) - wrote a couple of transpilers
